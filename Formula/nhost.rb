89fdf843831e905fbea4c28ecc9c740f7ba48494
David Barroso <dbarrosop@dravetech.com>
2026-03-26 10:43:19 +0000

    Update nhost to 1.41.0

--- Formula/nhost.rb
+++ Formula/nhost.rb
@@ -4,30 +4,30 @@
 class Nhost < Formula
   desc "Nhost CLI for local development, project management, and deployments"
   homepage "https://github.com/nhost/nhost"
-  version "1.40.0"
+  version "1.41.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/nhost/nhost/releases/download/cli@#{version}/cli-#{version}-darwin-arm64.tar.gz"
-      sha256 "94566266b136130125bd625eb09fb060827b6da3696294d73ce24fb1de4bf225"
+      sha256 "4a42f81af0ddc2294d970b7e391b7e01eaa00a9e6aba2ef6428c7a698c168a36"
     end
 
     on_intel do
       url "https://github.com/nhost/nhost/releases/download/cli@#{version}/cli-#{version}-darwin-amd64.tar.gz"
-      sha256 "87a56bed5634bde5da16e7f0151fd5b6660b7d16a088838f0ce7399e2d50c4af"
+      sha256 "4a8d57230a4e8135548649e2126de008e364f2843b7d64237322f306056372ec"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/nhost/nhost/releases/download/cli@#{version}/cli-#{version}-linux-arm64.tar.gz"
-      sha256 "c449ea308733fed5189f134087e1e5608b55fb2e7ed6775c24841f79e6ed62a6"
+      sha256 "5db79e7ab62128f9d63c25124e779a7e6af4e15ca349f330477fe73d9ff02d18"
     end
 
     on_intel do
       url "https://github.com/nhost/nhost/releases/download/cli@#{version}/cli-#{version}-linux-amd64.tar.gz"
-      sha256 "fa80540a1b334434b4fb1918f3e0ecbb867bccf48a8899646c5832a5363aae6e"
+      sha256 "f9431b4279777ef0b42af728e1c0dac5db2cb1d74fa9470ea662581eb7311c61"
     end
   end
 
